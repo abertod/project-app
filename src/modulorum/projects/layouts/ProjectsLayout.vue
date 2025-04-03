f93229b646b4fa2f8584d1d6db49b6613e232e92
master <master@dib-e026-imac20.dib.upv.es>
2025-04-03 17:39:23 +0200

    Creando el menu de la interfaz

---
+++ src/modulorum/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <SupraMenu />
+  <LateralisMenu />
+  <main><RouterView /><!--Eslo mismo que <router-view />  --></main>
+</template>
+
+<script lang="ts" setup>
+import SupraMenu from "@/modulorum/commune/components/SupraMenu.vue"
+import LateralisMenu from "../components/LateralisMenu.vue"
+</script>
