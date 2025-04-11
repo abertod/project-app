75dd0723c6d060f4889108f4a367034d9b4db167
master <master@dib-e026-imac20.dib.upv.es>
2025-04-11 17:15:29 +0200

    Instalación Pinia y creación de Store

--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -83,10 +83,13 @@ import PropriumModal from '@/modulorum/commune/components/PropriumModal.vue';
 import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
 import ModalIcon from '@/modulorum/commune/icons/ModalIcon.vue';
 import { ref } from 'vue';
+import { useProjectsStore } from '../store/projects.store';
 
 const modalAperta = ref(false);
 
 const propriumModalAperta = ref(false);
+
+const projectsStore = useProjectsStore();
 
 const cumNovusValorem = (projectNomen: string) => {
   console.log(projectNomen);
